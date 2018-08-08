d68492843aad7e42a569dbd08b98634efd44e3c1
Danilo Luz <danilo.luz@climatempo.com.br>
2018-08-07 22:42:49 -0300

    Adicionado template engine nunjucks
    - Importado biblioteca nunjuck
    - Adicionado configuracao do nunjucks
    - Adicionado comentarios ao codigo do arquivo index.js
    - Adicionado view de preview index.njk

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+/*!
+ * snippetfy
+ * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
+ * MIT Licensed
+ */
+
+/**
+ * Dependencia de modulos.
+ */
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
 
+/**
+ * Definindo Express
+ */
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+/**
+ * Configurando template engine nunjucks definindo o diretorio das views
+ */
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
 });
 
+/**
+ * Definindo extensão do arquivo usado na view engine
+ */
+app.set('view engine', 'njk');
+
+/**
+ * Configuração do body parser
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * Rota de teste
+ */
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+/**
+ * Iniciando aplicação na porta 3000
+ */
 app.listen(3000);
