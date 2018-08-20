7bc9219f1c5e2bd4b82f99bd3d9a17f0789b1ff9
Danilo Luz <danilo.luz@climatempo.com.br>
2018-08-20 19:29:02 -0300

    Criado model User
    - Criado a model user com os parametros que irao receber os dados para criar os registros na tabela User

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+/*!
+ * Snippetfy
+ *
+ * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
+ * version 0.1.0
+ */
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  });
+
+  return User;
+};
