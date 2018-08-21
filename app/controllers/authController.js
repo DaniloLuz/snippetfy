cc58132d79bda0529384019734d4fa61436427a6
Danilo Luz <danilo-2108@hotmail.com>
2018-08-21 10:57:56 -0300

    Criado rota /register
    - Criado rota register para fazer cadastro de novos usuários
    - Adicionado biblioteca bcryptjs e utilizado para criptografar a senha
      do usuário ao salvar no banco de dados

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,8 +2,10 @@
  * Snippetfy
  *
  * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
- * version 0.3.0
+ * version 0.4.0
  */
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
 
 module.exports = {
   /**
@@ -22,5 +24,29 @@ module.exports = {
    */
   signup(req, res) {
     return res.render('auth/signup');
+  },
+
+  /**
+   * Método para criar novo registro
+   * @param {Request} req
+   * @param {Reponse} res
+   */
+  async register(req, res) {
+    const { email } = req.body;
+
+    // Verifica se já existe um usuário com este registro
+    if (await User.findOne({ where: { email } })) {
+      // Volta para a rota que estava antes de chegar neste método
+      return res.redirect('back');
+    }
+
+    // Fazendo criptografia da senha digitada
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    // Passa as informações digitadas no formulário para criar novo registro
+    await User.create({ ...req.body, password });
+
+    // Faz redirect
+    return res.redirect('/');
   },
 };
--- app/routes.js
+++ app/routes.js
@@ -2,18 +2,19 @@
  * Snippetfy
  *
  * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
- * version 0.2.0
+ * version 0.3.0
  */
 const express = require('express');
 
 // Utilizando o Router do express para trabalhar com a rotas
 const routes = express.Router();
-const userController = require('./controllers/authController');
+const authController = require('./controllers/authController');
 
 /**
  * Rota /
  */
-routes.get('/', userController.signin);
-routes.get('/signup', userController.signup);
+routes.get('/', authController.signin);
+routes.get('/signup', authController.signup);
+routes.post('/register', authController.register)
 
 module.exports = routes;
