33b6a9859ea956f5c20f812c1185226e6069e0ee
Danilo Luz <danilo-2108@hotmail.com>
2018-08-21 13:42:31 -0300

    Criado rota app/dashboard
    - Adicionado middleware de verificacao em todas as rotas que começam com
      /app

---
+++ app/controllers/dashboardController.js
@@ -0,0 +1,11 @@
+/*!
+ * Snippetfy
+ *
+ * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
+ * version 0.1.0
+ */
+module.exports = {
+  index(req, res) {
+    return res.render('dashboard/index');
+  },
+};
--- app/routes.js
+++ app/routes.js
@@ -2,13 +2,15 @@
  * Snippetfy
  *
  * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
- * version 0.4.0
+ * version 0.5.0
  */
 const express = require('express');
 
 // Utilizando o Router do express para trabalhar com a rotas
 const routes = express.Router();
 const authController = require('./controllers/authController');
+const dashboardController = require('./controllers/dashboardController');
+const authMiddleware = require('./middlewares/auth');
 
 /**
  * Middleware de mensagens flash
@@ -18,6 +20,13 @@ routes.use((req, res, next) => {
   res.locals.flashError = req.flash('error');
   next();
 });
+
+/**
+ * Definindo que todas as rotas /app precisam passar pelo
+ * middleware de verificação de sessão
+ */
+routes.use('/app', authMiddleware);
+
 /**
  * Rota /
  */
@@ -25,5 +34,6 @@ routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);
+routes.get('/app/dashboard', dashboardController.index);
 
 module.exports = routes;
