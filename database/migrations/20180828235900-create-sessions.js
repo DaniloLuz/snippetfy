01186d398be3a3c64c831ac4b66fb08b0a21c81e
Danilo Luz <danilo.luz@climatempo.com.br>
2018-08-28 21:17:06 -0300

    Criado migration Session para criar tabela no banco

---
+++ database/migrations/20180828235900-create-sessions.js
@@ -0,0 +1,31 @@
+/*!
+ * Snippetfy
+ *
+ * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
+ * version 0.1.0
+ */
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    queryInterface.createTable('Sessions', {
+      sid: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      expires: DataTypes.DATE,
+      data: DataTypes.TEXT,
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    queryInterface.dropTable('Sessions');
+  },
+};
