8a0a455921c237f3938977226037417e2b09f388
Danilo Luz <danilo.luz@climatempo.com.br>
2018-08-28 21:16:10 -0300

    Adicionado configuracao do connect-session-sequelize
    - Feito a configuracao do pacote connect-session-sequelize com o Sequelize

--- config/session.js
+++ config/session.js
@@ -1,5 +1,19 @@
+/*!
+ * Snippetfy
+ *
+ * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
+ * version 0.2.0
+ */
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('../app/models');
+
 module.exports = {
   secret: 'snippetfy2018devteam',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false, // Não cria a sessão no banco de dados
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
