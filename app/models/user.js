1115ebca99343743fc78209ee2fb0918698a50d7
Danilo Luz <danilo.luz@climatempo.com.br>
2018-09-13 22:14:53 -0300

    Adicionado associacao entre model User e Category
    Adicionado associacao de um para muito, na qual um usuario pode ter varias categorias

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@
  * Snippetfy
  *
  * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
- * version 0.1.0
+ * version 0.2.0
  */
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -10,6 +10,11 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
   });
+
+  User.associate = (models) => {
+    // Uma categoria pode ter vários snippets
+    User.hasMany(models.Category);
+  };
 
   return User;
 };
