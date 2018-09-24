e18e0412307bdc114a85c7980b4e1d143fd415d3
Danilo Luz <danilo.luz@climatempo.com.br>
2018-09-23 23:30:57 -0300

    Criado dashboad com listagem de categorias
    Foi criado uma interface para a listagem das categoris criadas no banco de dados

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -2,10 +2,23 @@
  * Snippetfy
  *
  * Copyright(c) 2018 Danilo Luz <danilo-2108@hotmail.com>
- * version 0.1.0
+ * version 0.2.0
  */
+const { Category, Snippet } = require('../models');
+
 module.exports = {
-  index(req, res) {
-    return res.render('dashboard/index');
+  async index(req, res, next) {
+    try {
+      const categories = await Category.findAll({
+        include: [Snippet], // Eager Load
+        where: {
+          UserId: req.session.user.id, // Retorna apenas as categorias do user
+        },
+      });
+
+      return res.render('dashboard/index', { categories });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
